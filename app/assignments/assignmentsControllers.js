1d1486a868b1acd7e4921026fb16d897b77195a9
Steve Rivera <srivera.x1@gmail.com>
2014-11-26 19:04:53 -0500

    assignments table now has view button within last column.

--- app/assignments/assignmentsControllers.js
+++ app/assignments/assignmentsControllers.js
@@ -1,19 +1,35 @@
-var acct = angular.module('assignmentsControllers', ['assignmentsFactory']);
+var acct = angular.module('assignmentsControllers', ['assignmentsFactory', 'ui.grid.selection']);
 
 acct.controller('assignmentsCtrl', ['$scope', 'assignmentsAPI', 'authInfo', function($scope, assignmentsAPI, authInfo){
     // http://ui-grid.info/docs/#/tutorial/205_row_editable
     // http://ui-grid.info/docs/#/tutorial/215_paging
     $scope.gridAssignments = {
         enableSorting: true,
+        enableRowSelection: true,
+        multiSelect: true,
         columnDefs: [
-            { name: 'Created By', field:'creator' },
-            { name: 'Date Added', field:'dateAdded' },
-            { name: 'Date Due', field:'dateDue' },
+            { name: 'Created By', field: 'creator' },
+            { name: 'Date Added', field: 'dateAdded' },
+            { name: 'Date Due', field: 'dateDue' },
+            { name: 'View', cellTemplate: '<button class="btn btn-default btn-xs" ng-click="getExternalScopes().loadBatch(row.entity)">View</button>' }
         ]
     };
     
     assignmentsAPI.getAll(authInfo.token).success(function(res){
         $scope.gridAssignments.data = res;
-        
     });
+    
+    $scope.gridAssignmentsScope = {
+        loadBatch: function(batchObj){
+            // redirect to batch viewer page.
+            // this is the same as any documents page.
+        }
+    };
+    
+    $scope.gridAssignments.onRegisterApi = function(gridApi){
+        $scope.gridApi = gridApi;
+        gridApi.selection.on.rowSelectionChanged($scope,function(row){
+            console.log(row.entity.batchID);
+        });
+    };
 }]);
