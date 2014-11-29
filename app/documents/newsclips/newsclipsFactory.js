98c3174b3e203d4da84041fbfdca6145fad25446
Steve Rivera <srivera.x1@gmail.com>
2014-11-29 16:57:06 -0500

    newsclip table adjusted.

--- app/documents/newsclips/newsclipsFactory.js
+++ app/documents/newsclips/newsclipsFactory.js
@@ -2,7 +2,7 @@ var nclip = angular.module('newsclipsFactory', []);
 
 nclip.factory('newsclipsAPI', ['$http', 'apiRoot', function($http, apiRoot){
 	var dataFactory = {};
-	var urlBase = apiRoot + '/documents/Newsclips';
+	var urlBase = apiRoot + '/documents/NewsClips';
 	
 	dataFactory.getAll = function(token){
 		return $http.get(urlBase + '?token=' + token);
