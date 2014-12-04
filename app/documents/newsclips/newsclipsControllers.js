332e793e35c80b6c5efedda29477c9a65425665f
Steve Rivera <srivera.x1@gmail.com>
2014-12-03 19:28:34 -0500

    added multi select functionality to newsclips view

--- app/documents/newsclips/newsclipsControllers.js
+++ app/documents/newsclips/newsclipsControllers.js
@@ -1,8 +1,10 @@
-var nclips = angular.module('newsclipsControllers', ['newsclipsFactory']);
+var newsclips = angular.module('newsclipsControllers', ['newsclipsFactory']);
 
-nclips.controller('newsclipsCtrl', ['$scope', 'newsclipsAPI', 'authInfo', function($scope, newsclipsAPI, authInfo){
+newsclips.controller('newsclipsCtrl', ['$scope', 'newsclipsAPI', 'authInfo', function($scope, newsclipsAPI, authInfo){
     $scope.gridNewsclips = {
-        enableSorting: true,
+        enableRowSelection: true,
+        enableSelectAll: true,
+        multiSelect: true,
         columnDefs: [
             { field: 'Headline' },
             { field: 'Abstract' },
@@ -13,4 +15,13 @@ nclips.controller('newsclipsCtrl', ['$scope', 'newsclipsAPI', 'authInfo', functi
     newsclipsAPI.getAll(authInfo.token).success(function(res){
         $scope.gridNewsclips.data = res;
     });
+    
+    $scope.gridApi.onRegisterApi = function(gridApi){ 
+        $scope.gridApi = gridApi;
+    };
+    $scope.myFunc = function(){
+        var selectedRows = $scope.gridApi.selection.getSelectedRows();
+        
+        for(var i=0; i<selectedRows.length; i++){}
+    };
 }]);
